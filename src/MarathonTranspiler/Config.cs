2a255909ff9a49e136e469b57e6c0acc384ec838
Scott Seto <scott.seto@outlook.com>
2025-02-15 17:51:32 -0500

    Add options.

---
+++ src/MarathonTranspiler/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MarathonTranspiler
+{
+    public class Config
+    {
+        [JsonPropertyName("transpilerOptions")]
+        public TranspilerOptions TranspilerOptions { get; set; }
+
+        [JsonPropertyName("include")]
+        public List<string> Include { get; set; }
+
+        [JsonPropertyName("exclude")]
+        public List<string> Exclude { get; set; }
+
+        [JsonPropertyName("rootDir")]
+        public string RootDirectory { get; set; }
+    }
+}
---
+++ src/MarathonTranspiler/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace MarathonTranspiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            // Path to the JSON file
+            string jsonFilePath = currentDirectory + "\\mrtconfig.json";
+
+            // Read and deserialize the JSON file
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            Config config = JsonSerializer.Deserialize<Config>(jsonContent);
+
+            // Create a Matcher instance
+            var matcher = new Matcher();
+
+            // Add include patterns
+            foreach (var includePattern in config.Include)
+            {
+                matcher.AddInclude(includePattern);
+            }
+
+            // Add exclude patterns
+            foreach (var excludePattern in config.Exclude)
+            {
+                matcher.AddExclude(excludePattern);
+            }
+
+            // Specify the root directory for the search
+            string rootDirectory = config.RootDirectory;
+            var directoryInfo = new DirectoryInfoWrapper(new DirectoryInfo(rootDirectory));
+
+            // Execute the matcher
+            var matchingResult = matcher.Execute(directoryInfo);
+
+            // Output the matched files
+            foreach (var file in matchingResult.Files)
+            {
+                var fullPath = Path.Combine(rootDirectory, file.Path);
+
+            }
+        }
+    }
+}
